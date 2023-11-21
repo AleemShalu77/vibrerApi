eb3e60b687f3285971919b4c74e44957f2244289
Abdul Aleem <aleem9860@gmail.com>
2023-11-21 21:51:16 +0530

    update user routes

--- module/userArtist/userArtistRoute.js
+++ module/userArtist/userArtistRoute.js
@@ -1,13 +1,29 @@
 const userArtistController = require("./userArtistController");
-const middleware = require("../../middleware")
+const middleware = require("../../middleware");
 
-module.exports = router => {
-    router.post("/artist/login", middleware.validateToken, userArtistController.artistLogin);
-    router.post("/artist/reset-password", middleware.validateToken, userArtistController.forgotPasswordArtist);
-    router.post("/add/UserArtist", middleware.validateToken, userArtistController.addUserArtist);
-    router.post("/update/UserArtistSpecificColumn", middleware.validateToken, userArtistController.updateUserArtistSpecificColumn);
-    router.put("/update/UserArtist", middleware.validateToken, userArtistController.updateUserArtist);
-    router.get("/all/UserArtist", middleware.validateToken, userArtistController.getAllUserArtist);
-    router.get("/UserArtist/:id", middleware.validateToken, userArtistController.getUserArtist);
-    router.post("/remove/UserArtist/:id", middleware.validateToken, userArtistController.deleteUserArtist);
-}
+module.exports = (router) => {
+  router.post("/loginUser", userArtistController.artistLogin);
+  router.post(
+    "/artist/reset-password",
+    middleware.validateToken,
+    userArtistController.forgotPasswordArtist
+  );
+  router.post("/registerUser", userArtistController.addUserArtist);
+  router.post(
+    "/update/UserArtistSpecificColumn",
+    middleware.validateToken,
+    userArtistController.updateUserArtistSpecificColumn
+  );
+  router.put("/update/UserArtist", userArtistController.updateUserArtist);
+  router.get(
+    "/all/UserArtist",
+    middleware.validateToken,
+    userArtistController.getAllUserArtist
+  );
+  router.get("/UserArtist/:id", userArtistController.getUserArtist);
+  router.post(
+    "/remove/UserArtist/:id",
+    middleware.validateToken,
+    userArtistController.deleteUserArtist
+  );
+};
